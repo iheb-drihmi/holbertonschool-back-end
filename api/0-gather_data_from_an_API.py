4ba36570ef1dc0166a7dd24a12494ec2648d0361
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-08-22 02:40:04 +0100

    tasks

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,26 +7,33 @@ his/her TODO list progress
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-
     if len(argv) != 2:
         exit()
 
     user_id = argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    user = requests.get(user_url).json()
+    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    user_response = requests.get(user_url)
 
-    todos_url = ('https://jsonplaceholder.typicode.com/users/{}/todos'
-                 .format(user_id))
-    todos = requests.get(todos_url).json()
+    if user_response.status_code != 200:
+        print("Error fetching user data.")
+        exit()
 
-    completed_tasks = [task.get('title') for task in todos
-                       if task.get('completed') is True]
-    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
-          len(completed_tasks), len(todos)))
+    user = user_response.json()
+
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+    todos_response = requests.get(todos_url)
+
+    if todos_response.status_code != 200:
+        print("Error fetching user's tasks.")
+        exit()
+
+    todos = todos_response.json()
+
+    completed_tasks = [task.get('title') for task in todos if task.get('completed') is True]
+    print(f"Employee {user.get('name')} is done with tasks({len(completed_tasks)}/{len(todos)}):")
     for task in completed_tasks:
-        print("\t {}".format(task))
+        print(f"\t{task}")
 
 
 
