8a4fe25ea389683e8471e5d0001afbd94686448d
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-08-29 17:35:20 +0100

    tasks

--- python_variable_annotations/1-concat.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Concat two strings together"""
-
-
-def concat(str1: str, str2: str) -> str:
-    """Concatenate two strings (str1, str2)"""
-    return str1 + str2
--- python_variable_annotations/2-floor.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Get the floor of the float"""
-
-
-def floor(n: float) -> int:
-    """Get the floor by converting to int"""
-    return int(n)
